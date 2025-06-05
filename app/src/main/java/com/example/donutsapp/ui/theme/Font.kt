833c7abc7faf3f44f6de1b7dedf379633b98c9bb
Thoraya <thoriahamdy99@gmail.com>
2025-06-05 14:52:52 +0300

    add fonts for GonutsWithDonuts screen

---
+++ app/src/main/java/com/example/donutsapp/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.example.donutsapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.donutsapp.R
+
+val InterFont = FontFamily(
+    Font(R.font.inter_bold, weight = FontWeight.Bold),
+    Font(R.font.inter_medium, weight = FontWeight.Medium),
+    Font(R.font.inter_semi_bold, weight = FontWeight.SemiBold)
+)
